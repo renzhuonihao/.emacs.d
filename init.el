7b2533ae98adfd23197ac9ef5903594477dba91b
任卓 <renzhuonihao@gmail.com>
2015-06-30 02:29:10 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,12 @@
+;;添加Emacs搜索路径
+(add-to-list 'load-path "~/.emacs.d/elisps")
+;(add-to-list 'load-path "~/test")
+;(add-to-list 'load-path "/tmp")
+
+
+;载入基本配置文件
+;(require 'base)       这两种载入方法都可以
+(load "base.el")
+
+;载入配色方案
+(require 'color-theme)
